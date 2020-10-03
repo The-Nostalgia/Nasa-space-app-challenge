f0bfb9a76ae0191eac6bf273b8c05d0b704e7f9c
abhilekh <abhilekhgautam119@gmail.com>
2020-10-03 18:47:14 +0545

    Svg and animations

---
+++ site/javascript/script.js
@@ -0,0 +1,6 @@
+buttonOne=document.getElementById('one');
+buttonTwo=document.getElementById('two');
+buttonThree=document.getElementById('three');
+buttonFour=document.getElementById('four');
+buttonFive=document.getElementById('five');
+buttonSix=document.getElementById('six');
