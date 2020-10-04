355f054ae3bb2cb66bc92b65995ef47183c5dc91
abhilekh <abhilekhgautam119@gmail.com>
2020-10-04 18:11:18 +0545

    Coding Completed Just articles Needed now

--- site/javascript/script.js
+++ site/javascript/script.js
@@ -19,9 +19,7 @@ partFour=document.getElementById('partFour');
 partFive=document.getElementById('partFive');
 partSix=document.getElementById('partSix');
 
-
-
-
+//for Desktop
 buttonOne.addEventListener('click',(e)=>{
     partOne.style.display =partOne.style.display==''?'inline':'';
     partTwo.style.display ='';
@@ -30,26 +28,47 @@ buttonOne.addEventListener('click',(e)=>{
     partFive.style.display =''
     partSix.style.display =''
 
-    // buttonOne.innerHtml=buttonOne.innerHtml=='Learn More'?'Close':'Learn More';
-    // console.log(buttonOne.innerHtml);
+    e.target.innerText=(e.target.innerText==='Learn More'?'Close':'Learn More');
+    buttonTwo.innerText='Learn More';
+    buttonThree.innerText='Learn More';
+    buttonFour.innerText='Learn More';
+    buttonFive.innerText='Learn More';
+    buttonSix.innerText='Learn More';
+
 })
-buttonTwo.addEventListener('click',()=>{
+buttonTwo.addEventListener('click',(e)=>{
     partTwo.style.display =partTwo.style.display==''?'inline':'';
     partOne.style.display ='';
     partThree.style.display ='';
     partFour.style.display ='';
     partFive.style.display ='';
     partSix.style.display ='';
+
+    e.target.innerText=(e.target.innerText==='Learn More'?'Close':'Learn More');
+    buttonOne.innerText='Learn More';
+    buttonThree.innerText='Learn More';
+    buttonFour.innerText='Learn More';
+    buttonFive.innerText='Learn More';
+    buttonSix.innerText='Learn More';
+
 })
-buttonThree.addEventListener('click',()=>{
+buttonThree.addEventListener('click',(e)=>{
     partThree.style.display =partThree.style.display==''?'inline':'';
     partOne.style.display ='';
     partTwo.style.display ='';
     partFour.style.display ='';
     partFive.style.display ='';
     partSix.style.display ='';
+
+    e.target.innerText=(e.target.innerText==='Learn More'?'Close':'Learn More');
+    buttonTwo.innerText='Learn More';
+    buttonOne.innerText='Learn More';
+    buttonFour.innerText='Learn More';
+    buttonFive.innerText='Learn More';
+    buttonSix.innerText='Learn More';
+
 })
-buttonFour.addEventListener('click',()=>{
+buttonFour.addEventListener('click',(e)=>{
     partFour.style.display =partFour.style.display==''?'inline':'';
     partOne.style.display ='';
     partTwo.style.display ='';
@@ -57,23 +76,49 @@ buttonFour.addEventListener('click',()=>{
     partFive.style.display ='';
     partSix.style.display ='';
 
+    e.target.innerText=(e.target.innerText==='Learn More'?'Close':'Learn More');
+    buttonTwo.innerText='Learn More';
+    buttonThree.innerText='Learn More';
+    buttonOne.innerText='Learn More';
+    buttonFive.innerText='Learn More';
+    buttonSix.innerText='Learn More';
+
+
 })
-buttonFive.addEventListener('click',()=>{
+buttonFive.addEventListener('click',(e)=>{
     partFive.style.display =partFive.style.display==''?'inline':'';
     partOne.style.display ='';
     partTwo.style.display ='';
     partThree.style.display ='';
     partFour.style.display ='';
     partSix.style.display ='';
+
+    e.target.innerText=(e.target.innerText==='Learn More'?'Close':'Learn More');
+    buttonTwo.innerText='Learn More';
+    buttonThree.innerText='Learn More';
+    buttonFour.innerText='Learn More';
+    buttonOne.innerText='Learn More';
+    buttonSix.innerText='Learn More';
+
 })
-buttonSix.addEventListener('click',()=>{
+buttonSix.addEventListener('click',(e)=>{
     partSix.style.display =partSix.style.display==''?'inline':'';
     partOne.style.display ='';
     partTwo.style.display ='';
     partThree.style.display ='';
     partFour.style.display ='';
     partFive.style.display ='';
+
+    e.target.innerText=(e.target.innerText==='Learn More'?'Close':'Learn More');
+    buttonTwo.innerText='Learn More';
+    buttonThree.innerText='Learn More';
+    buttonFour.innerText='Learn More';
+    buttonFive.innerText='Learn More';
+    buttonOne.innerText='Learn More';
+
 })
+
+//for mbl
 mblOne.addEventListener('click',(e)=>{
     partOne.style.display =partOne.style.display==''?'inline':'';
     partTwo.style.display ='';
@@ -82,26 +127,47 @@ mblOne.addEventListener('click',(e)=>{
     partFive.style.display =''
     partSix.style.display =''
 
-    // buttonOne.innerHtml=buttonOne.innerHtml=='Learn More'?'Close':'Learn More';
-    // console.log(buttonOne.innerHtml);
+    e.target.innerText=(e.target.innerText==='Learn More'?'Close':'Learn More');
+    buttonTwo.innerText='Learn More';
+    buttonThree.innerText='Learn More';
+    buttonFour.innerText='Learn More';
+    buttonFive.innerText='Learn More';
+    buttonSix.innerText='Learn More';
+
 })
-mblTwo.addEventListener('click',()=>{
+mblTwo.addEventListener('click',(e)=>{
     partTwo.style.display =partTwo.style.display==''?'inline':'';
     partOne.style.display ='';
     partThree.style.display ='';
     partFour.style.display ='';
     partFive.style.display ='';
     partSix.style.display ='';
+
+    e.target.innerText=(e.target.innerText==='Learn More'?'Close':'Learn More');
+    buttonTwo.innerText='Learn More';
+    buttonThree.innerText='Learn More';
+    buttonFour.innerText='Learn More';
+    buttonFive.innerText='Learn More';
+    buttonSix.innerText='Learn More';
+
 })
-mblThree.addEventListener('click',()=>{
+mblThree.addEventListener('click',(e)=>{
     partThree.style.display =partThree.style.display==''?'inline':'';
     partOne.style.display ='';
     partTwo.style.display ='';
     partFour.style.display ='';
     partFive.style.display ='';
     partSix.style.display ='';
+
+    e.target.innerText=(e.target.innerText==='Learn More'?'Close':'Learn More');
+    mblTwo.innerText='Learn More';
+    mblOne .innerText='Learn More';
+    mblFour.innerText='Learn More';
+    mblFive.innerText='Learn More';
+    mblSix.innerText='Learn More';
+
 })
-mblFour.addEventListener('click',()=>{
+mblFour.addEventListener('click',(e)=>{
     partFour.style.display =partFour.style.display==''?'inline':'';
     partOne.style.display ='';
     partTwo.style.display ='';
@@ -109,20 +175,44 @@ mblFour.addEventListener('click',()=>{
     partFive.style.display ='';
     partSix.style.display ='';
 
+    e.target.innerText=(e.target.innerText==='Learn More'?'Close':'Learn More');
+    mblTwo.innerText='Learn More';
+    mblThree.innerText='Learn More';
+    mblOne.innerText='Learn More';
+    mblFive.innerText='Learn More';
+    mblSix.innerText='Learn More';
+
+
 })
-mblFive.addEventListener('click',()=>{
+mblFive.addEventListener('click',(e)=>{
     partFive.style.display =partFive.style.display==''?'inline':'';
     partOne.style.display ='';
     partTwo.style.display ='';
     partThree.style.display ='';
     partFour.style.display ='';
     partSix.style.display ='';
+
+    e.target.innerText=(e.target.innerText==='Learn More'?'Close':'Learn More');
+    mblTwo.innerText='Learn More';
+    mblThree.innerText='Learn More';
+    mblFour.innerText='Learn More';
+    mblOne.innerText='Learn More';
+    mblSix.innerText='Learn More';
+
 })
-mblSix.addEventListener('click',()=>{
+mblSix.addEventListener('click',(e)=>{
     partSix.style.display =partSix.style.display==''?'inline':'';
     partOne.style.display ='';
     partTwo.style.display ='';
     partThree.style.display ='';
     partFour.style.display ='';
     partFive.style.display ='';
+
+    e.target.innerText=(e.target.innerText==='Learn More'?'Close':'Learn More');
+    mblTwo.innerText='Learn More';
+    mblThree.innerText='Learn More';
+    mblFour.innerText='Learn More';
+    mblFive.innerText='Learn More';
+    mblOne.innerText='Learn More';
+
 })
